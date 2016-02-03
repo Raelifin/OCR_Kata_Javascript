3a6f55dc52a0798bbe87e1404c4deccf5a4c2f3c
Raelifin <raelifin@gmail.com>
2016-02-03 06:58:56 -0500

    Added test coverage

--- tests.js
+++ tests.js
@@ -1,6 +1,12 @@
-var errorCase1 = "This is not supposed to work!";
-var errorCase2 = ''+
+var errorCase0 = "This is not supposed to work!";
+var errorCase1 = ''+
 ''+"\n"+
+'| || || || || || || || || |'+"\n"+
+'|_||_||_||_||_||_||_||_||_|'+"\n"+
+'                           ';
+
+var useCase0 = ''+
+' _  _  _  _  _  _  _  _  _ '+"\n"+
 '| || || || || || || || || |'+"\n"+
 '|_||_||_||_||_||_||_||_||_|'+"\n"+
 '                           ';
@@ -9,18 +15,55 @@ var useCase1 = ''+
 ' _  _  _  _  _  _  _  _  _ '+"\n"+
 '| || || || || || || || || |'+"\n"+
 '|_||_||_||_||_||_||_||_||_|'+"\n"+
+'                           '+"\n"+
+'                           '+"\n"+
+'  |  |  |  |  |  |  |  |  |'+"\n"+
+'  |  |  |  |  |  |  |  |  |'+"\n"+
+'                           '+"\n"+
+' _  _  _  _  _  _  _  _  _ '+"\n"+
+' _| _| _| _| _| _| _| _| _|'+"\n"+
+'|_ |_ |_ |_ |_ |_ |_ |_ |_ '+"\n"+
+'                           '+"\n"+
+' _  _  _  _  _  _  _  _  _ '+"\n"+
+' _| _| _| _| _| _| _| _| _|'+"\n"+
+' _| _| _| _| _| _| _| _| _|'+"\n"+
+'                           '+"\n"+
+'                           '+"\n"+
+'|_||_||_||_||_||_||_||_||_|'+"\n"+
+'  |  |  |  |  |  |  |  |  |'+"\n"+
+'                           '+"\n"+
+' _  _  _  _  _  _  _  _  _ '+"\n"+
+'|_ |_ |_ |_ |_ |_ |_ |_ |_ '+"\n"+
+' _| _| _| _| _| _| _| _| _|'+"\n"+
+'                           '+"\n"+
+' _  _  _  _  _  _  _  _  _ '+"\n"+
+'|_ |_ |_ |_ |_ |_ |_ |_ |_ '+"\n"+
+'|_||_||_||_||_||_||_||_||_|'+"\n"+
+'                           '+"\n"+
+' _  _  _  _  _  _  _  _  _ '+"\n"+
+'  |  |  |  |  |  |  |  |  |'+"\n"+
+'  |  |  |  |  |  |  |  |  |'+"\n"+
+'                           '+"\n"+
+' _  _  _  _  _  _  _  _  _ '+"\n"+
+'|_||_||_||_||_||_||_||_||_|'+"\n"+
+'|_||_||_||_||_||_||_||_||_|'+"\n"+
+'                           '+"\n"+
+' _  _  _  _  _  _  _  _  _ '+"\n"+
+'|_||_||_||_||_||_||_||_||_|'+"\n"+
+' _| _| _| _| _| _| _| _| _|'+"\n"+
 '                           ';
 
 
 QUnit.test("Rejects misshapen input", function( assert ) {
 	assert.throws(function(){
+		ocrDivideInputFile(errorCase0);
+	}, "Error Case 0 Passed!");
+	assert.throws(function(){
 		ocrDivideInputFile(errorCase1);
 	}, "Error Case 1 Passed!");
-	assert.throws(function(){
-		ocrDivideInputFile(errorCase2);
-	}, "Error Case 2 Passed!");
 });
 
 QUnit.test("Accepts well-shaped input", function( assert ) {
-	assert.ok(ocrDivideInputFile(useCase1) != null, "Passed!");
+	assert.ok(ocrDivideInputFile(useCase0) != null, "Use Case 0 Passed!");
+	assert.ok(ocrDivideInputFile(useCase1) != null, "Use Case 1 Passed!");
 });
