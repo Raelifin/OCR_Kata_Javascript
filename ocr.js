be94af4bfdeb2549d455564313eee4c334ce8b5c
Raelifin <raelifin@gmail.com>
2016-02-03 06:50:19 -0500

    Accepts well-formed input
    Also reformatted the test cases to handle newlines more explicitly and
    be more standards-compliant.

--- ocr.js
+++ ocr.js
@@ -1,3 +1,18 @@
 function ocrDivideInputFile(fileAsString) {
-	throw "This function hasn't been written!";
+	var rawLines = fileAsString.split(/\r?\n/);
+	if (rawLines.length % 4 != 0) {
+		throw "Misshapen input. File has "+rawLines.length+". Expecting a multiple of 4.";
+	}
+	var resultLines = [];
+	for (var i=0; i < rawLines.length; i += 4) {
+		var resultLine = [];
+		for (var j=0; j < 4; j++) {
+			if (rawLines[i+j].length != 27) {
+				throw "Misshapen input. Line "+(i+j)+" has "+rawLines[i+j].length+" characters. Expecting 27. ("+rawLines[i+j]+")";
+			}
+			resultLine.push(rawLines[i+j]);
+		}
+		resultLines.push(resultLine);
+	}
+	return resultLines;
 }
--- tests.js
+++ tests.js
@@ -1,15 +1,26 @@
-var errorCase1 = " This is not supposed to work!";
+var errorCase1 = "This is not supposed to work!";
+var errorCase2 = ''+
+''+"\n"+
+'| || || || || || || || || |'+"\n"+
+'|_||_||_||_||_||_||_||_||_|'+"\n"+
+'                           ';
 
-var useCase1 = "\
- _  _  _  _  _  _  _  _  _ \
-| || || || || || || || || |\
-|_||_||_||_||_||_||_||_||_|\
-                           ";
-						   
-useCase1 = useCase1.substring(1,useCase1.length); //Trim the initial newlines. (Which are added for ease of reading source code.)
+var useCase1 = ''+
+' _  _  _  _  _  _  _  _  _ '+"\n"+
+'| || || || || || || || || |'+"\n"+
+'|_||_||_||_||_||_||_||_||_|'+"\n"+
+'                           ';
+
 
 QUnit.test("Rejects misshapen input", function( assert ) {
 	assert.throws(function(){
 		ocrDivideInputFile(errorCase1);
-	}, "Passed!");
+	}, "Error Case 1 Passed!");
+	assert.throws(function(){
+		ocrDivideInputFile(errorCase2);
+	}, "Error Case 2 Passed!");
+});
+
+QUnit.test("Accepts well-shaped input", function( assert ) {
+	assert.ok(ocrDivideInputFile(useCase1) != null, "Passed!");
 });
